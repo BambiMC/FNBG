cefa15d7ff512e9bf482a939ea7329349815b584
BambiMC <fabians.viper@arcor.de>
2022-09-24 12:56:07 +0200

    text color changes

--- test.php
+++ test.php
@@ -18,7 +18,7 @@
                     <div class="timeline-2 left-2">
                         <div class="card">
                             <img src="res/imgs/bpanda-demo.png" class="card-img-top" alt="BPanda Demo Bild">
-                            <div class="card-body p-4">
+                            <div class="card-body p-4 text-orange">
                                 <h4 class="fw-bold mb-1">Bachelorarbeit in Zusammenarbeit mit MID GmbH</h4>
                                 <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i>Ab November 2022</p>
                                 <p class="mb-0">Component Testing als Hilfsmittel zur Erhöhung der User Experience und Quality of
@@ -29,7 +29,7 @@
                     <div class="timeline-2 left-2">
                         <div class="card">
                             <img src="res/imgs/dsag.webp" class="card-img-top" alt="DSAG Bild">
-                            <div class="card-body p-4">
+                            <div class="card-body p-4 text-orange">
                                 <h4 class="fw-bold mb-1">Kooperation mit der DSAG</h4>
                                 <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i>Laufend</p>
                                 <p class="mb-0">Schreiben eines Expertenbeitrags sowie eine Online-Session zum Thema UI/UX & Usability</p>
@@ -39,7 +39,7 @@
                     <div class="timeline-2 left-2">
                         <div class="card">
                             <img src="res/imgs/summit.jpg" class="card-img-top" alt="Responsive image">
-                            <div class="card-body p-4">
+                            <div class="card-body p-4 text-orange">
                                 <h4 class="fw-bold mb-1">Teilnahme am Big-Data.AI Summit & HUB.Berlin</h4>
                                 <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i>April 2019 & Juni 2022</p>
                                 <p class="mb-0"></p>
@@ -49,7 +49,7 @@
                     <div class="timeline-2 left-2">
                         <div class="card">
                             <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(125).webp" class="card-img-top" alt="Responsive image">
-                            <div class="card-body p-4">
+                            <div class="card-body p-4 text-orange">
                                 <h4 class="fw-bold mb-1"> Quis autem vel eum voluptate</h4>
                                 <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i> 2014</p>
                                 <p class="mb-0">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
@@ -62,7 +62,7 @@
                     <div class="timeline-2 left-2">
                         <div class="card">
                             <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(144).webp" class="card-img-top" alt="Responsive image">
-                            <div class="card-body p-4">
+                            <div class="card-body p-4 text-orange">
                                 <h4 class="fw-bold mb-1">Mussum ipsum cacilds</h4>
                                 <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i> 2013</p>
                                 <p class="mb-0">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
