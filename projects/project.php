da12e5eaac9c1eb1343a79c9001f9332358fc02d
fnberger <f.berger@mid.de>
2023-04-29 17:54:13 +0200

    fixing project.php include structure

--- projects/project.php
+++ projects/project.php
@@ -2,11 +2,11 @@
 <html color-mode="light" lang="en">
 
 
-<?php include(__DIR__ . '/../res/php/head.php');?>
+<?php include(__DIR__ . '/res/php/head.php');?>
 
 <body class="z-0">
 
-    <?php include(__DIR__ . '/../res/php/header.php');?>
+    <?php include(__DIR__ . '/res/php/header.php');?>
 
     <main class="container mx-auto p-10">
 
@@ -19,7 +19,7 @@
 
     </main>
 
-    <?php include(__DIR__ . '/../res/php/footer.php');?>
+    <?php include(__DIR__ . '/res/php/footer.php');?>
 </body>
 
 
