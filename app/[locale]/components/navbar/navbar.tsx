ba98eb9f12aac26ad84db16dad686239f08d49f2
fnberger <fabians.viper@arcor.de>
2023-08-29 17:02:36 +0200

    more fixes, json loading slow, but working

--- app/[locale]/components/navbar/navbar.tsx
+++ app/[locale]/components/navbar/navbar.tsx
@@ -27,7 +27,7 @@ const Navigation = () => {
     <ThemeSwitcher/>
       <button data-mobile-menu className="inline px-2 py-3 md:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="24" height="24"
-          viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+          viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
           <line x1="4" y1="6" x2="20" y2="6"></line>
           <line x1="4" y1="12" x2="20" y2="12"></line>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Navbar from './components/navbar/navbar';
-
 const Homepage: React.FC = () => {
   return (
-    <div>
-      <Navbar/>
+    <main className='p-20 mt-20'>
+    <div className="flex justify-around items-center mr-32">
+      <img className="" src="/W1.png" alt="picture of Fabian Berger" width={600} height={24}/>
+
+        <p className="">{"Hey, I'm Fabian Berger, a 20 year old student from Germany. I'm currently studying computer science at the University of Applied Sciences in Darmstadt. I'm interested in web development, machine learning and data science."}
+        </p>
     </div>
+
+
+    </main>
   );
 };
 
