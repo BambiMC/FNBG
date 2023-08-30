fd82b2781f8f4a9a5be7e03f8b64d739a29bdb3a
BambiMC <fabians.viper@arcor.de>
2023-08-30 23:36:15 +0200

    language loader separated and build into a few pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
+"use client"
+import React from 'react';
+import { useLanguageData } from '@/data/languageLoader';
+
 const Homepage: React.FC = () => {
+  const data = useLanguageData();
+
   return (
     <main className='p-20 mt-20'>
-    <div className="flex justify-around items-center mr-32">
-      <img className="" src="/W1.png" alt="picture of Fabian Berger" width={600} height={24}/>
+      {data && (
+        <div className="flex justify-around items-center mr-32">
+          <img className="" src="/W1.png" alt="picture of Fabian Berger" width={600} height={24} />
 
-        <p className="">{"Hey, I'm Fabian Berger, a 23 year old student from Germany. I'm currently studying computer science at the Friedrich Alexander University in Erlangen."}
-        </p>
-    </div>
+          <p>Email: {data['welcomeText']}</p>
 
+        </div>
 
-    </main>
+      )}
+    </main >
   );
 };
 
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,34 +1,17 @@
 "use client"
-import React, { useEffect, useState } from 'react';
-
-interface UserData {
-  [key: string]: any;
-}
+import React from 'react';
+import { useLanguageData } from '@/data/languageLoader';
 
 const MyPage: React.FC = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const lang = localStorage.getItem('lang');
-    const language = lang || 'en';
-
-    import(`../../data/${language}.json`)
-      .then((importedData) => {
-        console.log(importedData);
-        setData(importedData);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  const data = useLanguageData();
 
   return (
     <div>
       <h1 className='mt-32'>User Information</h1>
       {data && (
         <div>
-          <p>Name: {data['welcome']}</p>
-          <p>Email: {data['about']}</p>
+          <p>Name: {data['welcomeText']}</p>
+          <p>Email: {data['imprint']}</p>
         </div>
       )}
     </div>
