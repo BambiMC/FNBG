34eabc54dcdb1e61148ae343ba2a43cce23ece28
fnberger <fabians.viper@arcor.de>
2023-09-04 17:26:32 +0200

    mobile navbar, footer is always on top now

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,17 @@ const Homepage: React.FC = () => {
     <div>
       {data && (
         <main className='overflow-visible p-20 mt-20'>
-          <div className="flex justify-evenly flex-col md:flex-row">
-            <img className=" w-5/6 lg:w-2/6 self-center" src="/W2.webp" alt={data['altPicture']} />
-            <p className="z-10 ml-4 w-5/6 lg:w-3/6 self-center p-12 font-bold text-2xl from-neutral-200" style={{ backgroundColor: "var(--bg-accent)" }}>
+          <div className="flex justify-center items-center flex-col md:flex-row">
+            <img className="w-5/6 lg:w-2/6" src="/W2.webp" alt={data['altPicture']} />
+            <p className="z-10 w-5/6 lg:w-3/6 p-12 font-bold text-2xl from-neutral-200 text-center" style={{ backgroundColor: "var(--bg-accent)" }}>
               {data['welcomeText']}
             </p>
             <div className='w-1/6'>
               <Triangle />
             </div>
           </div>
-
-        </main >
+        </main>
       )}
-
     </div>
   );
 };
